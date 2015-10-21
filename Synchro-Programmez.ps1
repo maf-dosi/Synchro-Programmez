b55e626abb5dd644881659699606bb34d872b078
Uposax <pascal.audoux@gmail.com>
2015-10-21 14:07:09 +0200

    Comparaison avec la liste deja downloadee

--- Synchro-Programmez.ps1
+++ Synchro-Programmez.ps1
@@ -13,8 +13,20 @@ $operation=$req.forms["user-login"].Fields["edit-submit"]
 $postParams = @{"name"=$user;"pass"=$password;"form_id"="user_login";"form_build_id"=$formId;"op"=$operation}
 
 $req = Invoke-WebRequest "http://www.programmez.com/user/" -Method POST -Body $postParams
-#$req
-#/user/36971/archives
-#$req.rawcontent
 $req = Invoke-WebRequest "http://www.programmez.com/user/36971/archives/"
-$req.Links | Where-Object {$_.innerText -like "*.pdf"}
+$archives = $req.Links | Where-Object {$_.innerText -like "*.pdf"}
+
+$archiveNames = New-Object System.Collections.ArrayList
+foreach($item in $archives.innerText) {
+	$item = "mag_pdf_" + $item
+	[void] $archiveNames.Add($item)
+}
+
+#$archiveNames
+$downloaded = Get-ChildItem -Path "S:\Etudes\Commun_Etudes\Livres\Programmez" -File | Where-Object -Filter {$PSItem.Name -like "mag_*.pdf"}
+
+foreach ($item in $archiveNames){
+	if ( $downloaded -notcontains $item ) {
+		$item
+	}
+}
